d40ac123bd5a062455001f9e7b46a782e617b16b
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2023-03-13 09:05:59 -0500

    Basic Interest Contratc

--- src/YieldFarm.sol
+++ src/YieldFarm.sol
@@ -1,38 +1,61 @@
+// SPX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 contract YieldFarm {
-    address owner;
-    uint256 poolAmount;
-    uint256 poolRate;
-    uint256 poolLockDuration;
+    address public owner;
+    uint256 public poolAmount;
+    uint256 public poolRate; // Annual interest rate, for simplicity
+    uint256 public poolLockDuration;
 
-    mapping (address => uint256) deposits;
+    struct Deposit {
+        uint256 amount;
+        uint256 time;
+    }
+
+    mapping(address => Deposit) public deposits;
 
     constructor() {
         owner = msg.sender;
         poolAmount = 0;
-        poolRate = 10;
-        poolLockDuration = 30;
+        poolRate = 10; // Representing 10% annual interest rate
+        poolLockDuration = 30 days;
     }
 
     function deposit() public payable {
         require(msg.value > 0, "Deposit required.");
         poolAmount += msg.value;
-        deposits[msg.sender] += msg.value;
+        deposits[msg.sender] = Deposit(msg.value, block.timestamp);
     }
 
     function withdraw() public {
-        uint256 withdrawalAmount = deposits[msg.sender] * poolRate / 100;
-        require(withdrawalAmount > 0, "No funds available.");
-        require(block.timestamp > poolLockDuration, "Pool is currently locked.");
+        Deposit memory userDeposit = deposits[msg.sender];
+        require(userDeposit.amount > 0, "No funds available.");
+        require(block.timestamp > userDeposit.time + poolLockDuration, "Pool is currently locked.");
+
+        uint256 depositDuration = block.timestamp - userDeposit.time;
+        uint256 interest = calculateInterest(userDeposit.amount, depositDuration);
+        uint256 withdrawalAmount = userDeposit.amount + interest;
         
+        // Ensure the contract has enough funds to cover the withdrawal
+        
+        uint256 contractBalance = address(this).balance;
+
+        require(contractBalance >= withdrawalAmount, "Contract does not have enough funds.");
+        require(poolAmount >= withdrawalAmount, "Pool does not have enough funds.");
+       
+        poolAmount -= withdrawalAmount;
+        delete deposits[msg.sender];
+
         (bool sent, ) = msg.sender.call{value: withdrawalAmount}("");
         require(sent, "Failed to send Ether");
-        
-        poolAmount -= withdrawalAmount;
-        deposits[msg.sender] = 0;
     }
 
+    function calculateInterest(uint256 amount, uint256 depositDuration) public view returns (uint256) {
+        uint256 interestPerYear = amount * poolRate / 100;
+        uint256 interestForDuration = (interestPerYear * depositDuration) / 365 days;
+        return interestForDuration;
+    }
+    
     function getPoolAmount() public view returns (uint256) {
         return poolAmount;
     }
--- test/YieldFarm.t.sol
+++ test/YieldFarm.t.sol
@@ -4,20 +4,73 @@ import "forge-std/Test.sol";
 import "../src/YieldFarm.sol";
 
 contract YieldFarmTest is Test {
-    YieldFarm yieldFarm;
+    YieldFarm public yieldFarm;
+    address public admin;
+    address public user;
+    uint256 public initialContractBalance = 10 ether;
+    uint256 public initialAdminDeposit = 5 ether; // Initial deposit by the admin
 
     function setUp() public {
         yieldFarm = new YieldFarm();
+        admin = address(1); // Simulated admin address
+        user = address(2); // Simulated regular user address
+
+        // Fund the YieldFarm contract to ensure it can cover interest payments
+        address payable contractAddr = payable(address(yieldFarm));
+        vm.deal(contractAddr, initialContractBalance);
+
+        // Admin makes an initial deposit to the contract
+        vm.startPrank(admin);
+        vm.deal(admin, initialAdminDeposit); // Provide admin with ETH
+        yieldFarm.deposit{value: initialAdminDeposit}();
+        vm.stopPrank();
+    }
+    
+    function testDeployment() public {
+        // Example test for contract deployment
+        assertEq(yieldFarm.owner(), address(this));
+        assertEq(yieldFarm.poolAmount(), initialAdminDeposit);
+        assertEq(yieldFarm.poolRate(), 10);
+        assertEq(yieldFarm.poolLockDuration(), 30 days);
     }
 
     function testDeposit() public {
-        // Example test for deposit function
-        address user = address(this);
-        uint256 depositAmount = 1 ether;
-        vm.deal(user, depositAmount); // Provide test contract with ETH
+        // Example test for user deposit function
+        uint256 userDepositAmount = 1 ether;
         vm.startPrank(user);
-        yieldFarm.deposit{value: depositAmount}();
-        assertEq(yieldFarm.getPoolAmount(), depositAmount);
+        vm.deal(user, userDepositAmount); // Provide user with ETH
+        yieldFarm.deposit{value: userDepositAmount}();
+        assertEq(yieldFarm.getPoolAmount(), initialAdminDeposit + userDepositAmount);
+        vm.stopPrank();
+    }
+    
+    function testInterestAccrual() public {
+        uint256 userDepositAmount = 1 ether;
+        vm.startPrank(user);
+        vm.deal(user, userDepositAmount); // Provide user with ETH for deposit
+        yieldFarm.deposit{value: userDepositAmount}();
+        
+        // Validate the pool amount has increased by the correct amount
+        assertEq(yieldFarm.getPoolAmount(), initialAdminDeposit + userDepositAmount);
+
+        // Simulate time passing: 365 days
+        vm.warp(block.timestamp + 365 days);
+
+
+        // Calculate the expected principle + interest amount
+        uint256 earnedInterest = userDepositAmount * yieldFarm.getPoolRate() / 100;
+        
+        // Calculate the expected total payout
+        uint256 totalPayout = userDepositAmount + earnedInterest;
+
+        // User attempts to withdraw, expecting interest on their deposit
+        yieldFarm.withdraw();        
+
+        // Check the balance to ensure it has increased by the correct interest amount
+        assertEq(address(user).balance, totalPayout);        
+        // Ensure the pool amount has decreased by the correct amount
+        assertEq(yieldFarm.getPoolAmount(), initialAdminDeposit - earnedInterest);
+        
         vm.stopPrank();
     }
 }
