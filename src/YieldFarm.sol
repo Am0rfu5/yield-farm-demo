b2354e202befcd475787163911fbfa3655982b2a
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2023-03-13 10:05:22 -0500

    events

--- src/YieldFarm.sol
+++ src/YieldFarm.sol
@@ -1,10 +1,10 @@
-// SPX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 contract YieldFarm {
     address public owner;
     uint256 public poolAmount;
-    uint256 public poolRate; // Annual interest rate, for simplicity
+    uint256 public poolRate;
     uint256 public poolLockDuration;
 
     struct Deposit {
@@ -14,40 +14,54 @@ contract YieldFarm {
 
     mapping(address => Deposit) public deposits;
 
+    // Define events
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event PoolRateChanged(uint256 newRate);
+    event PoolLockDurationChanged(uint256 newDuration);
+    
+    error PoolLocked(string message);
+    error NoFunds(string message);
+
     constructor() {
         owner = msg.sender;
         poolAmount = 0;
-        poolRate = 10; // Representing 10% annual interest rate
-        poolLockDuration = 30 days;
+        poolRate = 10; // 10% annual interest rate
+        poolLockDuration = 365 days;
     }
 
     function deposit() public payable {
         require(msg.value > 0, "Deposit required.");
         poolAmount += msg.value;
         deposits[msg.sender] = Deposit(msg.value, block.timestamp);
+
+        emit Deposited(msg.sender, msg.value);
     }
 
     function withdraw() public {
         Deposit memory userDeposit = deposits[msg.sender];
-        require(userDeposit.amount > 0, "No funds available.");
-        require(block.timestamp > userDeposit.time + poolLockDuration, "Pool is currently locked.");
+        if (userDeposit.amount > 0) {
+            revert NoFunds("No funds available.");
+        }
+        if (block.timestamp > userDeposit.time + poolLockDuration) {
+            revert PoolLocked("Pool is currently locked.");
+        }
 
         uint256 depositDuration = block.timestamp - userDeposit.time;
         uint256 interest = calculateInterest(userDeposit.amount, depositDuration);
         uint256 withdrawalAmount = userDeposit.amount + interest;
-        
-        // Ensure the contract has enough funds to cover the withdrawal
-        
-        uint256 contractBalance = address(this).balance;
 
+        uint256 contractBalance = address(this).balance;
         require(contractBalance >= withdrawalAmount, "Contract does not have enough funds.");
         require(poolAmount >= withdrawalAmount, "Pool does not have enough funds.");
-       
+
         poolAmount -= withdrawalAmount;
         delete deposits[msg.sender];
 
         (bool sent, ) = msg.sender.call{value: withdrawalAmount}("");
         require(sent, "Failed to send Ether");
+
+        emit Withdrawn(msg.sender, withdrawalAmount);
     }
 
     function calculateInterest(uint256 amount, uint256 depositDuration) public view returns (uint256) {
@@ -71,10 +85,12 @@ contract YieldFarm {
     function changePoolRate(uint256 newRate) public {
         require(msg.sender == owner, "Only owner function.");
         poolRate = newRate;
+        emit PoolRateChanged(newRate);
     }
 
     function changePoolLockDuration(uint256 newDuration) public {
         require(msg.sender == owner, "Only owner can change pool lock duration.");
         poolLockDuration = newDuration;
+        emit PoolLockDurationChanged(newDuration);
     }
 }
